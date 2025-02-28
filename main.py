fb01a1caa9b0735f121cf573b81a0daa66906334
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-02-28 21:09:08 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+
+
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mock.db"
+app.config["SECRET_KEY"] = "c0013adc9440afb24b0e40c0e0d3274c"
