609dfc8a2814735df967884d1866c20da02bdb88
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-03-01 09:27:42 +0100

    update main.py

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ from form import LoginForm, SignupForm, FileUpload
 from dotenv import load_dotenv
 import os
 
-
+# loading and validating variables
 load_dotenv(".env")
+
+def validate_env():
+	variables = ["SECRET_KEY", "DATABASE_URL"]
+	for var in variables:
+		if not os.getenv(var):
+			raise RuntimeError(f"{var} is not set, check your enviroment variable")
+			
 
 # flask configuration settings
 app=Flask(__name__)
+validate_env()
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.permanent_session_lifetime = timedelta(days=30)
+app.permanent_session_lifetime = timedelta(days=1)
 
-assert app.config["SQLALCHEMY_DATABASE_URI"] != None
-assert app.config["SECRET_KEY"] != None
 
+# wrapper to restrict access to login necessary areas
 def login_required(f):
-	# wrapper to restrict access to login necessary areas
 	@wraps(f)
 	def wrapped_function(*args, **kwargs):
 		if "username" not in session:
@@ -28,14 +34,15 @@ def login_required(f):
 	return wrapped_function
 
 
+# routing function
+
 @app.route("/login")
 def login():
 	login = LoginForm()
-	if login.validate_on_submit:
-		pass
-
-@app.get("/")
+	if login.validate_on_submit():
+		
 @app.get("/dashboard/")
+@app.get("/")
 @login_required
 def dashboard():
 	pass
